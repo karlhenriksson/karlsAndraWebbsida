4ce2056a6af9317c7d29d11d89c28c21fb357fcb
karlhenriksson <153213827+karlhenriksson@users.noreply.github.com>
2024-05-03 09:19:43 +0200

    Add files via upload

--- loadStaticData.js
+++ loadStaticData.js
@@ -17,18 +17,45 @@ const notices = {
     "Man kan lägga in text, bilder, <a href='404.html'>länkar</a>, och typ allt annat i en notis. Dock behöver den vara någorlunda liten för att passa i designen, och den måste alltid ha en titel som set ser ut just nu.",
 };
 
+const navigationOptions = [
+  ["Hem", "index.html"],
+  ["Navigera", "navigate.html"],
+  ["Kontakt", "contact.html"],
+];
+
 const sideBarDiv = document.getElementById("side-bar-div");
 
-// For all notices...
-for (let [title, content] of Object.entries(notices)) {
+function createNotice(title, content) {
   // Create a new HTML div with the right classes
   const noticeDiv = document.createElement("div");
   noticeDiv.classList =
     "bordered-primary padded rounded primary-highlight margin-y";
   // Add the notice content and title
   noticeDiv.innerHTML = `<h2>${title}</h2>
-  <p>${content}</p>`;
+<p>${content}</p>`;
   sideBarDiv.appendChild(noticeDiv);
+}
+
+// Add one notice if on phone, and all of them if on computer
+if (window.innerWidth < 800) {
+  const [title, content] = Object.entries(notices)[0];
+  createNotice(title, content);
+} else {
+  for (const [title, content] of Object.entries(notices)) {
+    createNotice(title, content);
+  }
+}
+
+// Add nav options
+for (const parent of document.getElementsByClassName("navigation-options")) {
+  for (const [name, url] of navigationOptions) {
+    const link = document.createElement("a");
+    link.innerText = name;
+    link.href = url;
+    link.classList = "navigation-option inverted-text-color";
+
+    parent.appendChild(link);
+  }
 }
 
 // Add a little icon to external links
